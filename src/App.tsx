ff48808c6c6b04433d120f0ebfab7167abcc9748
Ralf Vogler <ralf.vogler@gmail.com>
2023-04-17 19:09:38 +0200

    cleanup, design fields, animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const m = [
+    [1, 1, 2, 1],
+    [3, 2, 1, 1],
+    [2, 5, 6, 8],
+    [4, 2, 2, 1],
+    [1, 1, 2, 7],
+  ];
+  const rows = m.length;
+  const cols = m[0].length;
+  const fields = m.flatMap(row => row.map(col => <Field exp={col}/>));
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>2248</h1>
+      <div className="Fields" style={{gridTemplateColumns: 'auto '.repeat(cols)}}>
+        {fields}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
 
-export default App
+const colors = [
+  // https://coolors.co/palette/ffadad-ffd6a5-fdffb6-caffbf-9bf6ff-a0c4ff-bdb2ff-ffc6ff-fffffc
+  ["ffadad", "ffd6a5", "fdffb6", "caffbf", "9bf6ff", "a0c4ff", "bdb2ff", "ffc6ff", "fffffc"],
+  // https://coolors.co/palette/eddcd2-fff1e6-fde2e4-fad2e1-c5dedd-dbe7e4-f0efeb-d6e2e9-bcd4e6-99c1de
+  ["eddcd2", "fff1e6", "fde2e4", "fad2e1", "c5dedd", "dbe7e4", "f0efeb", "d6e2e9", "bcd4e6", "99c1de"],
+  // https://coolors.co/palette/eae4e9-fff1e6-fde2e4-fad2e1-e2ece9-bee1e6-f0efeb-dfe7fd-cddafd
+  ["eae4e9", "fff1e6", "fde2e4", "fad2e1", "e2ece9", "bee1e6", "f0efeb", "dfe7fd", "cddafd"],
+];
+const color = colors[2];
+
+const Field = ({ exp = 1}) => {
+  const text = 2**exp;
+  return <button className="Field" style={{backgroundColor: '#'+color[exp-1]}}>{text}</button>;
+};
